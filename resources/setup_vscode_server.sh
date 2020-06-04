f01b8ad9829a676b584459af694e86f3540237e2
chilcano <chilcano@intix.info>
2020-06-04 02:21:39 +0200

    VSCode Server guide added

--- resources/setup_vscode_server.sh
+++ resources/setup_vscode_server.sh
@@ -8,13 +8,13 @@ NOW=$(date +"%y%m%d.%H%M%S")
 VSCS_USER_DIR="${HOME}/.local/share/code-server/User"
 printf "\n==> Look&Feel and config \n"
 
-echo "> Getting 'settings.json' and copying to '$VSCS_USER_DIR/settings.json'"
+echo ">> Getting 'settings.json' and copying to '$VSCS_USER_DIR/settings.json'"
 wget -q https://raw.githubusercontent.com/chilcano/how-tos/master/resources/vscode/dot_config/Code/User/settings.json
 if [ -f "${VSCS_USER_DIR}/settings.json" ]; then
-    printf "\t Making a backup of '${VSCS_USER_DIR}/settings.json'\n"
+    printf ">> Making a backup of '${VSCS_USER_DIR}/settings.json'\n"
     mv ${VSCS_USER_DIR}/settings.json ${VSCS_USER_DIR}/settings.json.bak.${NOW} 
 fi 
-printf "\t Copying 'settings.json' to '${VSCS_USER_DIR}/'\n"
+printf ">> Copying 'settings.json' to '${VSCS_USER_DIR}/'\n"
 cp settings.json $VSCS_USER_DIR/.
 rm -f settings.json
 
@@ -30,7 +30,7 @@ code-server --install-extension yzhang.markdown-all-in-one
 code-server --install-extension mauve.terraform
 
 ## https://marketplace.visualstudio.com/items?itemName=VisualStudioExptTeam.vscodeintellicode
-code-server --install-extension VisualStudioExptTeam.vscodeintellicode
+######code-server --install-extension VisualStudioExptTeam.vscodeintellicode
 
 ## https://marketplace.visualstudio.com/items?itemName=redhat.java
 code-server --install-extension redhat.java
@@ -51,7 +51,7 @@ code-server --install-extension redhat.vscode-yaml
 code-server --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 
 ## https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-ssh
-code-server --install-extension ms-vscode-remote.remote-ssh
+######code-server --install-extension ms-vscode-remote.remote-ssh
 
 ## https://marketplace.visualstudio.com/items?itemName=ballerina.ballerina
 code-server --install-extension ballerina.ballerina
