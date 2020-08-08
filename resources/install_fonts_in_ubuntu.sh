1cdba8473fbdd5c1d63ebec1b20d6e3bbdb0402e
Chilcano <chilcano@intix.info>
2020-08-08 14:44:40 +0100

    Install fonts updated

--- resources/install_fonts_in_ubuntu.sh
+++ resources/install_fonts_in_ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 NOW2=$(date +"%y%m%d.%H%M%S")
-FONTS_DIR="${HOME}/.local/share/fonts"
+#FONTS_DIR="${HOME}/.local/share/fonts"
+FONTS_DIR="${HOME}/.fonts"
 
 if [ -d "${FONTS_DIR}/chilcano" ]; then
   printf "==> There are fonts in '${FONTS_DIR}/chilcano/', then backing up it.\n\n"
