b059b153a0bb825e511205ff0c5dfc1e6d482ee3
Chilcano <chilcano@intix.info>
2020-08-08 12:01:37 +0100

    Fonts script updated

--- resources/install_fonts_in_ubuntu.sh
+++ resources/install_fonts_in_ubuntu.sh
@@ -4,7 +4,7 @@ NOW2=$(date +"%y%m%d.%H%M%S")
 FONTS_DIR="~/.local/share/fonts"
 
 if [ -d "${FONTS_DIR}/chilcano" ]; then
-  printf ">> There are fonts in '${FONTS_DIR}/chilcano/', then backing up it.\n"
+  printf "==> There are fonts in '${FONTS_DIR}/chilcano/', then backing up it.\n\n"
   tar -zcvf "${FONTS_DIR}/chilcano-fonts.${NOW2}" "${FONTS_DIR}/chilcano"
   rm -rf "${FONTS_DIR}/.uuid"
   rm -rf "${FONTS_DIR}/chilcano/"
@@ -17,19 +17,19 @@ printf "==> Installing 'Menlo for Powerline' fonts. \n"
 git clone --depth=1 https://github.com/abertsch/Menlo-for-Powerline
 rm -rf ./Menlo-for-Powerline/.git
 mv "Menlo-for-Powerline" "${FONTS_DIR}/chilcano/"
-printf ">> Fonts updated/installed. \n\n"
+printf "Fonts updated/installed. \n\n"
 
 ## Ref: https://github.com/diogocavilha/fancy-git
 printf "==> Installing 'SourceCode+Powerline+Awesome+Regular' fonts. \n"
 wget -q https://github.com/diogocavilha/fancy-git/blob/master/fonts/SourceCodePro%2BPowerline%2BAwesome%2BRegular.ttf
 mv SourceCodePro+Powerline+Awesome+Regular.ttf "${FONTS_DIR}/chilcano/"
-printf ">> Fonts updated/installed. \n\n"
+printf "Fonts updated/installed. \n\n"
 
 ## Ref: https://github.com/ryanoasis/nerd-fonts#option-6-ad-hoc-curl-download
 printf "==> Installing 'Droid Sans Mono for Powerline Nerd' fonts. \n"
 wget -q https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 mv "'Droid Sans Mono Nerd Font Complete.otf'" "${FONTS_DIR}/chilcano/"
-printf ">> Fonts updated/installed. \n\n"
+printf "Fonts updated/installed. \n\n"
 
 sudo apt install -y gnome-tweaks
 printf "==> Now with Gnome-Tweaks select the patched font to use. \n\n"
