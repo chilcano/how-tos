41f4a62881f4e8e4bdb9c24c43f40cff153cfc2f
chilcano <chilcano@intix.info>
2020-12-15 21:12:40 +0100

    Guacamole install updated

--- resources/guacamole_install.sh
+++ resources/guacamole_install.sh
@@ -13,15 +13,15 @@ sudo apt install -y build-essential pkg-config libx11-dev libxkbfile-dev libsecr
 # guacamole dependencies
 sudo apt install -y make gcc g++ libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libssl-dev libvorbis-dev libwebp-dev
 
-printf ">> Installing tomcat9 \n"
+printf ">> Installing tomcat9 \n\n"
 sudo apt install -y tomcat9 tomcat9-admin tomcat9-common tomcat9-user
 
-printf ">> Starting tomcat9 \n"
+printf ">> Starting tomcat9 \n\n"
 # systemctl --user restart code-server
 sudo systemctl enable tomcat9
 sudo systemctl start tomcat9
 
-printf ">> Installing Guacamole Server and Client \n"
+printf ">> Installing Guacamole Server and Client \n\n"
 GUACAMOLE_VERSION="1.2.0"
 GUACAMOLE_SERVER="guacamole-server"
 GUACAMOLE_CLIENT="guacamole"
@@ -31,9 +31,9 @@ GUACAMOLE_BUNDLE_URL_SERVER="https://downloads.apache.org/guacamole/${GUACAMOLE_
 GUACAMOLE_BUNDLE_URL_CLIENT="https://downloads.apache.org/guacamole/${GUACAMOLE_VERSION}/binary/${GUACAMOLE_BUNDLE_CLIENT}.war"
 
 if [ -f "${GUACAMOLE_BUNDLE_SERVER}.tar.gz" ]; then
-  printf ">> The $GUACAMOLE_BUNDLE_SERVER.tar.gz file exists. Nothing to download. \n"
+  printf ">> The $GUACAMOLE_BUNDLE_SERVER.tar.gz file exists. Nothing to download. \n\n"
 else
-  printf ">> Downloading the $GUACAMOLE_BUNDLE_SERVER.tar.gz file. \n"
+  printf ">> Downloading the $GUACAMOLE_BUNDLE_SERVER.tar.gz file. \n\n"
   wget -q $GUACAMOLE_BUNDLE_URL_SERVER
 fi
 ## removing previous installation
@@ -48,26 +48,29 @@ sudo make
 sudo make install 
 sudo ldconfig
 
-printf ">> Starting Guacamole Server \n"
+printf ">> Starting Guacamole Server \n\n"
 sudo systemctl enable guacd
 sudo systemctl start guacd
 
 if [ -f "${GUACAMOLE_BUNDLE_CLIENT}.war" ]; then
-  printf ">> The $GUACAMOLE_BUNDLE_CLIENT.war file exists. Nothing to download. \n"
+  printf ">> The $GUACAMOLE_BUNDLE_CLIENT.war file exists. Nothing to download. \n\n"
 else
-  printf ">> Downloading the $GUACAMOLE_BUNDLE_CLIENT.war file. \n"
+  printf ">> Downloading the $GUACAMOLE_BUNDLE_CLIENT.war file. \n\n"
   wget -q $GUACAMOLE_BUNDLE_URL_CLIENT
 fi
 sudo rm -rf /etc/guacamole
 sudo mkdir /etc/guacamole
 sudo  mv $GUACAMOLE_BUNDLE_CLIENT.war /etc/guacamole/guacamole.war
-sudo  ln -s /etc/guacamole/guacamole.war /var/lib/tomcat9/webapps/
+## force the creation of a symbolic link
+sudo  ln -sf /etc/guacamole/guacamole.war /var/lib/tomcat9/webapps/
+## remove existing guacamole webapp
+sudo rm -rf /var/lib/tomcat9/webapps/guacamole/
 
-printf ">> Starting Guacamole \n"
+printf ">> Starting Guacamole \n\n"
 sudo systemctl restart tomcat9
 sudo systemctl restart guacd
 
-printf ">> Configuring Guacamole \n"
+printf ">> Configuring Guacamole \n\n"
 
 cat <<EOF > guacamole.properties
 guacd-hostname: localhost
@@ -84,24 +87,34 @@ sudo mkdir /etc/guacamole/{extensions,lib}
 sudo sh -c "echo \"GUACAMOLE_HOME=/etc/guacamole\" >> /etc/default/tomcat9"
 
 printf ">> Creating Guacamole users \n"
-GUACAMOLE_USER_RND=$(openssl rand -base64 32)
+#GUACAMOLE_USER_RND=$(openssl rand -base64 32)
+GUACAMOLE_USER_RND="demodemo"
 GUACAMOLE_USER_MD5=$(echo -n $GUACAMOLE_USER_RND | md5sum | cut -d ' ' -f 1)
 
 ## create a new user-mapping.xml
 cat <<EOF > user-mapping.xml
 <user-mapping>
-    <authorize 
-            username="admin"
-            password="${GUACAMOLE_USER_MD5}"
-            encoding="md5">
+  <authorize username="admin" password="${GUACAMOLE_USER_MD5}" encoding="md5">
+    <connection name="VNC">
+      <protocol>vnc</protocol>
+      <param name="hostname">192.168.1.53</param>
+      <param name="port">5901</param>
+      <param name="password">VNCPASS</param>
+    </connection>
 
-        <connection name="Ubuntu20.04-Server">
-            <protocol>ssh</protocol>
-            <param name="hostname">192.168.1.53</param>
-            <param name="port">22</param>
-            <param name="username">root</param>
-        </connection>
-    </authorize>
+    <connection name="Windows Server">
+      <protocol>rdp</protocol>
+      <param name="hostname">192.168.10.53</param>
+      <param name="port">3389</param>
+    </connection>
+
+    <connection name="Ubuntu20.04-Server">
+      <protocol>ssh</protocol>
+      <param name="hostname">192.168.1.53</param>
+      <param name="port">22</param>
+      <param name="username">chilcano</param>
+    </connection>
+  </authorize>
 </user-mapping>
 EOF
 sudo chown -R root:root user-mapping.xml
