1e4893ebc7da7030fe1c29e49d9684376f8539cb
Chilcano <chilcano@intix.info>
2021-01-05 16:30:39 +0100

    Jekyll script updated

--- resources/jekyll_setting_in_linux.sh
+++ resources/jekyll_setting_in_linux.sh
@@ -6,11 +6,11 @@ echo "##########################################################"
 echo "####              Setting Jekyll in Linux             ####"
 echo "##########################################################"
 
-echo "==> Installing Ruby"
-sudo apt-get update
-sudo apt -y install ruby ruby-dev build-essential zlib1g-dev
+printf "==> Installing Ruby \n"
+sudo apt -qq update
+sudo apt -yqq install ruby ruby-dev build-essential zlib1g-dev
 
-echo "==> Configuring Ruby Gems into Linux Home"
+printf "==> Configuring Ruby Gems into Linux Home \n"
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
@@ -18,16 +18,16 @@ source ~/.bashrc
 printf "\t Ruby version: $(ruby -v)\n"
 printf "\t Gem version: $(gem -v)\n"
 
-echo "==> Installing Jekyll with Bundler"
+printf "==> Installing Jekyll with Bundler \n"
 gem install bundler
 
-echo "==> Cloning the GitHub Page site and moving to project directory"
+printf "==> Cloning the GitHub Page site and moving to project directory \n"
 CURRENT_DIR=$PWD
 GIT_USER="chilcano"
 GIT_REPO="ghpages-holosec"
 GIT_PARENT_DIR="${HOME}/gitrepos"
 if [ -f "${GIT_PARENT_DIR}/${GIT_REPO}/README.md" ]; then
-    printf "\t The '${GIT_PARENT_DIR}/${GIT_REPO}' GitHub repo exists and contains files. Nothing to do.\n"
+    printf "\t The '${GIT_PARENT_DIR}/${GIT_REPO}' GitHub repo exists and contains files. Nothing to do. \n"
 else 
     printf "\t Cloning the '${GIT_REPO}' GitHub repo.\n"
     mkdir -p ${GIT_PARENT_DIR}; cd ${GIT_PARENT_DIR} 
@@ -35,26 +35,29 @@ else
 fi 
 cd ${GIT_PARENT_DIR}/${GIT_REPO}
 
-echo "==> Setting Gems local repository"
+printf "==> Setting Gems local repository \n"
 ###bundle install --path vendor/bundle
 bundle config set path vendor/bundle
 
-printf "==> Installing Jekyll and all Gems present in Gemfile in '${GIT_PARENT_DIR}/${GIT_REPO}'\n"
-bundle install
+printf "==> Installing Jekyll and all Gems present in Gemfile in '${GIT_PARENT_DIR}/${GIT_REPO}' \n"
+bundle install --quiet
 
-echo "==> Checking if Jekyll was installed properly. You should see 'jekyll 4.0.0'"
+printf "\n==> Checking if Jekyll was installed properly. You should see 'jekyll 4.0.0' \n"
 bundle exec jekyll -v
 
-echo "==> Serving a site with Jekyll"
+printf "\n==> Serving a site with Jekyll \n"
 cd $CURRENT_DIR
 
-printf "\t If you are using Google Analytics:\n"
+printf "\t If you are using Google Analytics uses 'JEKYLL_ENV=production': \n"
 printf "\t $ JEKYLL_ENV=production bundle exec jekyll serve --incremental --watch \n\n"
 
-printf "\t If you have posts in draft (place your posts in '<site>\_drafts\' folder without 'date' and 'permalink' in the front-matter):\n"
+printf "\t Running with 'JEKYLL_ENV=production' and listening on '0.0.0.0':\n"
+printf "\t $ JEKYLL_ENV=production bundle exec jekyll serve --incremental --watch --host=0.0.0.0 \n\n"
+
+printf "\t If you have posts in draft (place your posts in '<site>\_drafts\' folder without 'date' and 'permalink' in the front-matter): \n"
 printf "\t $ JEKYLL_ENV=production bundle exec jekyll serve --watch --drafts \n\n"
 
-printf "\t If you want run Jekyll without warnings:\n"
-printf "\t $ RUBYOPT=-W0 JEKYLL_ENV=production bundle exec jekyll serve --incremental --watch  \n\n"
+printf "\t If you want run Jekyll without warnings: \n"
+printf "\t $ RUBYOPT=-W0 JEKYLL_ENV=production bundle exec jekyll serve --incremental --watch \n\n"
 
 printf "\n\t** Duration of process: $((($(date +%s)-${TIME_RUN_DEVOPS}))) seconds **\n\n"
