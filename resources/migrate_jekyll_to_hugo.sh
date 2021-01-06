3cabfce32ebaf7f9d9ce2a8f167fbcf09cb921bf
Chilcano <chilcano@intix.info>
2021-01-06 13:40:02 +0100

    Migration Jekyll to Hugo bash updated

--- resources/migrate_jekyll_to_hugo.sh
+++ resources/migrate_jekyll_to_hugo.sh
@@ -5,21 +5,34 @@ echo "###############################################################"
 echo "#          Migrating existing Jekyll site to Hugo             #"
 echo "###############################################################"
 
+DIR_CURRENT=$PWD
+GIT_USER="chilcano"
 DIR_GITREPOS="gitrepos"
 DIR_SOURCE_JEKYLL="ghpages-holosec"
 DIR_TARGET_HUGO="ghpages-holosecio"
-DIR_CURRENT=$PWD
 
 HUGO_THEME_URL="https://github.com/spf13/herring-cove.git"
 HUGO_THEME_NAME="${HUGO_THEME_URL##*/}"
 
+if [ -f "${HOME}/${DIR_GITREPOS}/${DIR_SOURCE_JEKYLL}/README.md" ]; then
+    printf "~~> The '${HOME}/${DIR_GITREPOS}/${DIR_SOURCE_JEKYLL}' GitHub repo exists and contains files. Nothing to do. \n"
+else 
+    printf "~~> Cloning the '${DIR_SOURCE_JEKYLL}' Jekyll GitHub Pages repo. \n"
+    mkdir -p ${HOME}/${DIR_GITREPOS}/${DIR_SOURCE_JEKYLL} 
+    git clone https://github.com/${GIT_USER}/${DIR_SOURCE_JEKYLL} ${HOME}/${DIR_GITREPOS}/${DIR_SOURCE_JEKYLL}
+fi 
+
+printf "~~> Cleaning existing '${DIR_TARGET_HUGO}' Hugo GitHub Pages repo. \n"
 rm -rf ${HOME}/${DIR_GITREPOS}/${DIR_TARGET_HUGO}
 mkdir -p ${HOME}/${DIR_GITREPOS}/${DIR_TARGET_HUGO}/themes/
+
 hugo import jekyll ${HOME}/${DIR_GITREPOS}/${DIR_SOURCE_JEKYLL} ${HOME}/${DIR_GITREPOS}/${DIR_TARGET_HUGO}
 
+printf "~~> Importing the '${HUGO_THEME_URL}' Hugo Theme. \n"
 git clone ${HUGO_THEME_URL} ${HOME}/${DIR_GITREPOS}/${DIR_TARGET_HUGO}/themes/${HUGO_THEME_NAME}
 
+printf "~~> Serving the Hugo site in the LAN. \n"
 cd ${HOME}/${DIR_GITREPOS}/${DIR_TARGET_HUGO}/
-
 # hugo server -D --bind=0.0.0.0 --baseURL=http://192.168.1.59:1313/ghpages-dpio/
-hugo server -D --bind=0.0.0.0 --theme=${HUGO_THEME_NAME}
+printf "\t hugo server -D --bind=0.0.0.0 --theme=${HUGO_THEME_NAME} \n"
+printf "\t cd ${DIR_CURRENT} \n\n"
